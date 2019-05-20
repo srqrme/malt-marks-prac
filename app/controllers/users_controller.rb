999612e3110731b2217ff6c094c9ac8f68aef11a
srqrme <srqrme@gmail.com>
2019-05-20 11:35:06 -0400

    add strong params to users_controller and update current_user and logged_in? methods in application_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,4 +14,10 @@ class UsersController < ApplicationController
                 render :new
             end
     end
+
+    private 
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password)
+    end
 end
