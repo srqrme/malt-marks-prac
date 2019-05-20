3f424862ba6c2a9923c5ea98df18372fabe5a461
srqrme <srqrme@gmail.com>
2019-05-20 15:51:34 -0400

    add create method

--- app/controllers/whiskeys_controller.rb
+++ app/controllers/whiskeys_controller.rb
@@ -11,4 +11,15 @@ class WhiskeysController < ApplicationController
     def show
         @whiskey = Whiskey.find_by(id: params[:id])
     end
+
+    def create
+        @whiskey = Whiskey.find_or_create_by(whiskey_params)
+        if @whiskey.valid?
+            @whiskey.name = @whiskey.full_name
+            @whiskey.save
+            redirect_to @whiskey
+        else
+            render :new
+        end
+    end
 end
