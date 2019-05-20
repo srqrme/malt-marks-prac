a97a96447f8f6ad00fae164b6169c280fc0ac333
srqrme <srqrme@gmail.com>
2019-05-20 15:48:39 -0400

    add show method

--- app/controllers/whiskeys_controller.rb
+++ app/controllers/whiskeys_controller.rb
@@ -7,4 +7,8 @@ class WhiskeysController < ApplicationController
     def new
         @whiskey = Whiskey.new
     end
+
+    def show
+        @whiskey = Whiskey.find_by(id: params[:id])
+    end
 end
