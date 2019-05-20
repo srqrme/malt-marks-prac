ac4ee1f4076ef042075f775948f76540af5b80ec
srqrme <srqrme@gmail.com>
2019-05-20 11:38:07 -0400

    add validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ApplicationRecord
 
     has_many :marks
     has_many :whiskeys, through: :marks
+
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :password, presence:true, 
+                         length: { minimum: 6 }
+    
+    has_secure_password
 end
