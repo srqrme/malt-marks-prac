8b5375d9c97c3021b79eb1cd5c715685f2deed9c
srqrme <srqrme@gmail.com>
2019-05-21 14:53:09 -0400

    add create method

--- app/controllers/marks_controller.rb
+++ app/controllers/marks_controller.rb
@@ -10,11 +10,28 @@ class MarksController < ApplicationController
 
     def new
         if params[:whiskey_id] && !Whiskey.exists?(params[:whiskey_id])
-            redirect_to whiskeys_path
+            redirect_to whiskeys_path            
+        elsif 
+            @mark = Mark.find_by(user_id: current_user.id, wine_id: params[:wine_id])
+            redirect_to whiskey_mark_path(@mark.whiskey_id, @mark)
         else
             @mark = Mark.new(:whiskey_id => params[:whiskey_id], :user_id => current_user.id)
         end 
     end
+
+    def create
+        if @mark = Mark.find_by(user_id: current_user.id, whiskey_id: params[:mark][:whiskey_id])
+            redirect_to whiskey_mark_path(@mark.whiskey_id, @mark)
+        else
+            @mark = Mark.new(mark_params)
+                if @mark.save
+                    redirect_to whiskey_mark_path(@mark.whiskey, @mark)
+                else
+                    render :new
+                end
+        end        
+    end
+    
 
     private
 
