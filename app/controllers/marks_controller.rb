46ef9e525e0579db260fe91477c8a560176a0e13
srqrme <srqrme@gmail.com>
2019-05-21 13:32:52 -0400

    add index method

--- app/controllers/marks_controller.rb
+++ app/controllers/marks_controller.rb
@@ -1,2 +1,12 @@
 class MarksController < ApplicationController
+
+    def index
+        if params[:whiskey_id]
+            @marks = Whiskey.find(params[:whiskey_id]).marks
+        else
+            @marks = Mark.all
+        end
+    end
+
+
 end
